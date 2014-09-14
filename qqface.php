a7d0454c18fb2ac8718973ab12b40410f858d76f
Wills <unofhy@qq.com>
2014-09-14 13:07:29 +0800

    移去冗余代码
    移去冗余代码

--- qqface.php
+++ qqface.php
@@ -3,14 +3,9 @@ $GLOBALS['qqface_maps']=array(
  "/::)", "/::~", "/::B", "/::|", "/:8-)", "/::<", "/::$", "/::X", "/::Z", "/::'(", "/::-|", "/::@", "/::P", "/::D", "/::O", "/::(", "/::+", "/:--b", "/::Q", "/::T", "/:,@P", "/:,@-D", "/::d", "/:,@o", "/::g", "/:|-)", "/::!", "/::L", "/::>", "/::,@", "/:,@f", "/::-S", "/:?", "/:,@x", "/:,@@", "/::8", "/:,@!", "/:!!!", "/:xx", "/:bye", "/:wipe", "/:dig", "/:handclap", "/:&-(", "/:B-)", "/:<@", "/:@>", "/::-O", "/:>-|", "/:P-(", "/::'|", "/:X-)", "/::*", "/:@x", "/:8*", "/:pd", "/:<W>", "/:beer", "/:basketb", "/:oo", "/:coffee", "/:eat", "/:pig", "/:rose", "/:fade", "/:showlove", "/:heart", "/:break", "/:cake", "/:li", "/:bome", "/:kn", "/:footb", "/:ladybug", "/:shit", "/:moon", "/:sun", "/:gift", "/:hug", "/:strong", "/:weak", "/:share", "/:v", "/:@)", "/:jj", "/:@@", "/:bad", "/:lvu", "/:no", "/:ok", "/:love", "/:<L>", "/:jump", "/:shake", "/:<O>", "/:circle", "/:kotow", "/:turn", "/:skip", "/:oY");
 function qqface_convert_html($text){	
 	return str_replace( $GLOBALS['qqface_maps'],
-		array_map("add_img_label",  
-		array_keys($GLOBALS['qqface_maps']) ),
-		array_htmlspecialchars_decode($text)
+		array_map("add_img_label", array_keys($GLOBALS['qqface_maps']) ),
+		htmlspecialchars_decode($text, ENT_QUOTES)
 	);
-}
-
-function array_htmlspecialchars_decode($v){
-	return htmlspecialchars_decode($v, ENT_QUOTES);
 }
 
 function add_img_label($v){
